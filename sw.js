21cf908e578ad5d4ba3471c92376a20e29461428
FCO JAVIER MARTINEZ SALAZAR <273547425+JavierNodoSedna@users.noreply.github.com>
2026-04-22 06:14:18 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'polos-v3';
+const ASSETS = ['./index.html', './manifest.json', './icon-32.png', './icon-180.png', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached ||
+      fetch(e.request).then(resp => {
+        if(resp.ok && e.request.url.startsWith(self.location.origin)){
+          const clone = resp.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return resp;
+      }).catch(() => {
+        if(e.request.destination === 'document') return caches.match('./index.html');
+      })
+    )
+  );
+});
